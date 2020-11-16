445569d8d6ce7c363e5977ff743a176deba38768
David Ataiyero <david-ataiyero@live.com>
2020-11-16 12:09:19 +0000

    button on page is now work and data change picked up by database

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -63,6 +63,7 @@ router.post('/details',async(req, res) => {
         console.log(error);
     })
     res.render('accountDetails')
+
 })
 
 
@@ -114,7 +115,6 @@ router.post('/account/create', async(req, res) => {
 
 router.post('/login', async(req, res) => {
     let {email, password} = req.body;
-     console.log('hello')
     if (!await UserModel.checkExists(email)) {
         res.render('login', {error: 'no email exist'})
         return;
